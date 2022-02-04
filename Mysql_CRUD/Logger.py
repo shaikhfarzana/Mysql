9c7a74c326c991ac38fb6e01076e5acd52fd724b
shaikhfarzana <shaikhfarzana226@gmail.com>
2022-02-05 02:23:33 +0530

    [Farzana] Refactor: Refactored Logger file for storing logs of other files.

--- Mysql_CRUD/Logger.py
+++ Mysql_CRUD/Logger.py
@@ -8,18 +8,22 @@
 
 
 import logging
-
+import sys
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
 
+file_handler = logging.FileHandler("mysql.log","a")
+stream_handler = logging.StreamHandler(sys.stdout)
 
-file_handler = logging.FileHandler('mysql.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.WARNING)
 file_handler.setLevel(logging.ERROR)
+
 file_handler.setFormatter(formatter)
 
 stream_handler = logging.StreamHandler()
@@ -27,3 +31,4 @@ stream_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
+
